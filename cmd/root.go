e7414cf308723837fff6b578dee642d79739b697
kakeetopius <pius.kakeeto@strathmore.edu>
2026-04-22 15:24:30 +0300

    Initial bootstrap by cobra-cli

---
+++ cmd/root.go
@@ -0,0 +1,60 @@
+// Package cmd is used for command line argument passing
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "subg",
+	Short: "A tool for searching and downloading movie or series subtitles.",
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.subg.yaml)")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := os.UserHomeDir()
+		cobra.CheckErr(err)
+		configDir, err := os.UserConfigDir()
+		cobra.CheckErr(err)
+
+		// Search config in home directory with name ".subg" (without extension).
+		viper.AddConfigPath(home)
+		viper.AddConfigPath(configDir)
+		viper.SetConfigType("toml")
+		viper.SetConfigName(".subg")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	}
+}
