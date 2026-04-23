64d1e937a210b99bcf349072d1d909829a6a09a2
kakeetopius <pius.kakeeto@strathmore.edu>
2026-04-24 00:16:00 +0300

    Debugging

--- cmd/search_cmd.go
+++ cmd/search_cmd.go
@@ -52,6 +52,7 @@ func SearchCmd() *cobra.Command {
 			searchOptions.Type = featureType
 
 			subtitles, err := opensubtitles.SearchSubtitle(searchOptions)
+			fmt.Println(subtitles)
 			if len(subtitles) < 1 {
 				if episode != 0 || season != 0 {
 					return fmt.Errorf("no results returned for %v Season %v Episode %v", args[0], season, episode)
